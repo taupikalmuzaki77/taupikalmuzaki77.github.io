7eb73d378c17d1979b38a9b049662d345b03058e
taupikalmuzaki77 <taupikalmuzaki404@gmail.com>
2025-11-02 13:36:09 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+  strings: [
+    "building website applications",
+    "playing games",
+    "reading something",
+  ],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 500,
+});
+// <!-- typed js effect ends -->
