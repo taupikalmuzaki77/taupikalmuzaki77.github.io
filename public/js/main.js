f1205e586954427250bbd52e5bd6b06386aa5a5a
taupikalmuzaki77 <taupikalmuzaki404@gmail.com>
2025-11-03 15:38:18 +0700

    ok

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".typingText", {
+    strings: ["Build Web App", "Play Games", "Read Comics"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+    pauseDuration: 1000,
+  });
+});
